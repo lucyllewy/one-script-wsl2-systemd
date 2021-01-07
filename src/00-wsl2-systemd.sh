deb9fdd2ca8ce3cb2e78ee0ae1820be84721d8e1
Daniel Llewellyn <daniel@bowlhat.net>
2021-01-07 00:56:52 +0000

    Initial version supporting Insider Builds >21286
    * Use `wsl.conf` setting to launch `systemd`
    * Simplify the `profile.d` script
    * Add manual installation instructions for a less opinionated setup
    
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- src/00-wsl2-systemd.sh
+++ src/00-wsl2-systemd.sh
@@ -1,17 +1,4 @@
-SYSTEMD_EXE="$(command -v systemd)"
-
-if [ -z "$SYSTEMD_EXE" ]; then
-        if [ -x "/usr/lib/systemd/systemd" ]; then
-                SYSTEMD_EXE="/usr/lib/systemd/systemd"
-        else
-                SYSTEMD_EXE="/lib/systemd/systemd"
-        fi
-fi
-
-SYSTEMD_EXE="$SYSTEMD_EXE --unit=multi-user.target" # snapd requires multi-user.target not basic.target
-SYSTEMD_PID="$(ps -eo pid=,args= | awk '$2" "$3=="'"$SYSTEMD_EXE"'" {print $1}')"
-
-if [ -z "$SYSTEMD_PID" ] || [ "$SYSTEMD_PID" -ne 1 ]; then
+if [ "$(ps -c -p 1 -o command=)" != "systemd" ]; then
         if [ -z "$SUDO_USER" ]; then
                 export > "$HOME/.profile-systemd"
         fi
@@ -20,32 +7,20 @@ if [ -z "$SYSTEMD_PID" ] || [ "$SYSTEMD_PID" -ne 1 ]; then
                 exec sudo /bin/sh "/etc/profile.d/00-wsl2-systemd.sh"
         fi
 
-        if [ ! -f /etc/environment.orig ]; then
-                cp /etc/environment /etc/environment.orig
-        else
-                cp /etc/environment.orig /etc/environment
-        fi
-        echo "WSL_INTEROP='$WSL_INTEROP'" >> /etc/environment
-        echo "DISPLAY='$(awk '/nameserver/ { print $2":0" }' /etc/resolv.conf)'" >> /etc/environment
-
-        if [ -z "$SYSTEMD_PID" ]; then
-                env -i /usr/bin/unshare --fork --mount-proc --pid -- sh -c "
-                        mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
-                        exec $SYSTEMD_EXE
-                       " &
-                while [ -z "$SYSTEMD_PID" ]; do
-                        SYSTEMD_PID="$(ps -eo pid=,args= | awk '$2" "$3=="'"$SYSTEMD_EXE"'" {print $1}')"
-                        sleep 1
-                done
+        if ! grep -q WSL_INTEROP /etc/environment; then
+                echo "WSL_INTEROP='/run/WSL/$(ls -r /run/WSL | head -n1)'" >> /etc/environment
         fi
 
-        exec /usr/bin/nsenter --all --target "$SYSTEMD_PID" -- su - "$SUDO_USER"
+        if ! grep -q DISPLAY /etc/environment; then
+                echo "DISPLAY='$(awk '/nameserver/ { print $2":0" }' /etc/resolv.conf)'" >> /etc/environment
+        fi
+
+        exec /usr/bin/nsenter --mount --pid --target "$(ps -C systemd -o pid= | head -n1)" -- su - "$SUDO_USER"
 fi
 
-unset SYSTEMD_EXE
-unset SYSTEMD_PID
-
-[ -f "$HOME/.profile-systemd" ] && source "$HOME/.profile-systemd"
+if [ -f "$HOME/.profile-systemd" ]; then
+        source "$HOME/.profile-systemd"
+fi
 
 if [ -d "$HOME/.wslprofile.d" ]; then
         for script in "$HOME/.wslprofile.d/"*; do
