72d034727a7709c891f9e7cf499546b1e1f2e165
Dani Llewellyn <dani@bowlhat.net>
2021-08-26 13:59:40 +0100

    Update install.ps1
    * Add non-Windows Terminal user fallback to `Format-Hyperlink` function.
    
    Signed-off-by: Dani Llewellyn <dani@bowlhat.net>

--- install.ps1
+++ install.ps1
@@ -382,6 +382,14 @@ function Format-Hyperlink {
         [string] $Label
     )
 
+    if (($PSVersionTable.PSVersion.Major -lt 6 -or $IsWindows) -and -not $Env:WT_SESSION) {
+        # Fallback for Windows users not inside Windows Terminal
+        if ($Label) {
+            return "$Label ($Uri)"
+        }
+        return "$Uri"
+    }
+
     if ($Label) {
         return "`e]8;;$Uri`e\$Label`e]8;;`e\"
     }
