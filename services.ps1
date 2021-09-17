ccdcd36c378ca6387fd38c43db5e59b692fd6caa
Dani Llewellyn <dani@bowlhat.net>
2021-09-17 21:20:40 +0100

    Update services.ps1
    Signed-off-by: Dani Llewellyn <dani@bowlhat.net>

--- services.ps1
+++ services.ps1
@@ -23,9 +23,25 @@ try {
 
 try {
     if (-not $NoGPG) {
-        Register-ScheduledJob -Name GPGAgent -Trigger (New-JobTrigger -AtLogOn) -RunNow -ScriptBlock {
-            & "${env:ProgramFiles(x86)}/GnuPG/bin/gpg-connect-agent.exe" /bye
+        $Opts = @{
+            WakeToRun=$false
+            StartIfNotIdle=$true
+            StartIfOnBatteries=$true
+            StopIfGoingOffIdle=$false
+            StopIfGoingOnBatteries=$false
+            RunWithoutNetwork=$true
+            MultipleInstancePolicy="StopExisting"
         }
+        Register-ScheduledJob `
+            -Name GPGAgent `
+            -Trigger (New-JobTrigger -AtLogOn) `
+            -ScheduledJobOption  $Opts `
+            -RunNow `
+            -ScriptBlock {
+                & "${env:ProgramFiles(x86)}/GnuPG/bin/gpg-connect-agent.exe" /bye
+            }
+        Set-ScheduledTask -TaskName GPGAgent -TaskPath Microsoft\Windows\PowerShell\ScheduledJobs -Principal (New-ScheduledTaskPrincipal -Logontype Interactive -Userid $env:USERNAME)
+        Start-ScheduledTask -TaskName GPGAgent -TaskPath Microsoft\Windows\PowerShell\ScheduledJobs
     }
 } catch {
     Write-Output $_
@@ -38,78 +54,93 @@ try {
 
 try {
     if (-not $NoKernel) {
-        Register-ScheduledJob -Name UpdateWSL2CustomKernel -Trigger (New-JobTrigger -AtLogOn) -ScheduledJobOption (New-ScheduledJobOption -RequireNetwork) -RunNow -ScriptBlock {
-            function Get-IniContent($FilePath)
-            {
-                $ini = @{}
-                switch -regex -file $FilePath
-                {
-                    "^\[(.+)\]" # Section
-                    {
-                        $section = $matches[1]
-                        $ini[$section] = @{}
-                        $CommentCount = 0
-                    }
-                    "^(;.*)$" # Comment
-                    {
-                        $value = $matches[1]
-                        $CommentCount = $CommentCount + 1
-                        $name = "Comment" + $CommentCount
-                        $ini[$section][$name] = $value
-                    }
-                    "(.+?)\s*=(.*)" # Key
-                    {
-                        $name,$value = $matches[1..2]
-                        $ini[$section][$name] = $value
-                    }
-                }
-                return $ini
-            }
-
-            function Out-IniFile($InputObject, $FilePath)
-            {
-                $outFile = New-Item -ItemType file -Path $Filepath -Force
-                foreach ($i in $InputObject.keys)
-                {
-                    if (!($($InputObject[$i].GetType().Name) -eq "Hashtable"))
-                    {
-                        #No Sections
-                        Add-Content -Path $outFile -Value "$i=$($InputObject[$i])"
-                    } else {
-                        #Sections
-                        Add-Content -Path $outFile -Value "[$i]"
-                        Foreach ($j in ($InputObject[$i].keys | Sort-Object))
-                        {
-                            if ($j -match "^Comment[\d]+") {
-                                Add-Content -Path $outFile -Value "$($InputObject[$i][$j])"
-                            } else {
-                                Add-Content -Path $outFile -Value "$j=$($InputObject[$i][$j])"
-                            }
-
-                        }
-                        Add-Content -Path $outFile -Value ""
-                    }
-                }
-            }
-
-            $latest = (Invoke-RestMethod -Uri 'https://api.github.com/repos/diddlesnaps/WSL2-Linux-Kernel/releases' -UseBasicParsing)[0]
-            $latest_version = [version]$latest.tag_name.Remove($latest.tag_name.IndexOf('-microsoft-snapd'))
-            $current_version = [version](Get-Content '"$env:APPDATA/wsl2-custom-kernel-version.txt"')
-            if ($latest_version -gt $current_version) {
-                $assets = $latest.assets | Where-Object {$_.name -Like '*-x86_64'}
-                Invoke-WebRequest -Uri $assets.browser_download_url -OutFile '"$env:APPDATA/wsl2-custom-kernel"'
-                if ($?) {
-                    Move-Item "$env:APPDATA/wsl2-custom-kernel.tmp" "$env:APPDATA/wsl2-custom-kernel" -Force
-                    $latest_version | Set-Content "$env:APPDATA/wsl2-custom-kernel-version.txt"
-                    $wslconfig = @{'wsl2'=@{'kernel'=''}}
-                    if (Test-Path("$env:USERPROFILE/.wslconfig")) {
-                        $wslconfig = Get-IniContent "$env:USERPROFILE/.wslconfig"
-                    }
-                    $wslconfig["wsl2"]["kernel"] = "$env:APPDATA\wsl2-custom-kernel".Replace('\', '\\')
-                    Out-IniFile $wslconfig "$env:USERPROFILE/.wslconfig"
-                }
-            }
+        $Opts = @{
+            WakeToRun=$false
+            StartIfNotIdle=$true
+            StartIfOnBatteries=$true
+            StopIfGoingOffIdle=$false
+            StopIfGoingOnBatteries=$false
+            RunWithoutNetwork=$false
+            MultipleInstancePolicy="StopExisting"
         }
+        Register-ScheduledJob `
+            -Name UpdateWSL2CustomKernel `
+            -Trigger (New-JobTrigger -AtLogOn) `
+            -ScheduledJobOption $Opts `
+            -ScriptBlock {
+                function Get-IniContent($FilePath)
+                {
+                    $ini = @{}
+                    switch -regex -file $FilePath
+                    {
+                        "^\[(.+)\]" # Section
+                        {
+                            $section = $matches[1]
+                            $ini[$section] = @{}
+                            $CommentCount = 0
+                        }
+                        "^(;.*)$" # Comment
+                        {
+                            $value = $matches[1]
+                            $CommentCount = $CommentCount + 1
+                            $name = "Comment" + $CommentCount
+                            $ini[$section][$name] = $value
+                        }
+                        "(.+?)\s*=(.*)" # Key
+                        {
+                            $name,$value = $matches[1..2]
+                            $ini[$section][$name] = $value
+                        }
+                    }
+                    return $ini
+                }
+
+                function Out-IniFile($InputObject, $FilePath)
+                {
+                    $outFile = New-Item -ItemType file -Path $Filepath -Force
+                    foreach ($i in $InputObject.keys)
+                    {
+                        if (!($($InputObject[$i].GetType().Name) -eq "Hashtable"))
+                        {
+                            #No Sections
+                            Add-Content -Path $outFile -Value "$i=$($InputObject[$i])"
+                        } else {
+                            #Sections
+                            Add-Content -Path $outFile -Value "[$i]"
+                            Foreach ($j in ($InputObject[$i].keys | Sort-Object))
+                            {
+                                if ($j -match "^Comment[\d]+") {
+                                    Add-Content -Path $outFile -Value "$($InputObject[$i][$j])"
+                                } else {
+                                    Add-Content -Path $outFile -Value "$j=$($InputObject[$i][$j])"
+                                }
+
+                            }
+                            Add-Content -Path $outFile -Value ""
+                        }
+                    }
+                }
+
+                $latest = (Invoke-RestMethod -Uri 'https://api.github.com/repos/diddlesnaps/WSL2-Linux-Kernel/releases' -UseBasicParsing)[0]
+                $latest_version = [version]$latest.tag_name.Remove($latest.tag_name.IndexOf('-microsoft-snapd'))
+                $current_version = [version](Get-Content '"$env:APPDATA/wsl2-custom-kernel-version.txt"')
+                if ($latest_version -gt $current_version) {
+                    $assets = $latest.assets | Where-Object {$_.name -Like '*-x86_64'}
+                    Invoke-WebRequest -Uri $assets.browser_download_url -OutFile '"$env:APPDATA/wsl2-custom-kernel"'
+                    if ($?) {
+                        Move-Item "$env:APPDATA/wsl2-custom-kernel.tmp" "$env:APPDATA/wsl2-custom-kernel" -Force
+                        $latest_version | Set-Content "$env:APPDATA/wsl2-custom-kernel-version.txt"
+                        $wslconfig = @{'wsl2'=@{'kernel'=''}}
+                        if (Test-Path("$env:USERPROFILE/.wslconfig")) {
+                            $wslconfig = Get-IniContent "$env:USERPROFILE/.wslconfig"
+                        }
+                        $wslconfig["wsl2"]["kernel"] = "$env:APPDATA\wsl2-custom-kernel".Replace('\', '\\')
+                        Out-IniFile $wslconfig "$env:USERPROFILE/.wslconfig"
+                    }
+                }
+            }
+            Set-ScheduledTask -TaskName UpdateWSL2CustomKernel -TaskPath Microsoft\Windows\PowerShell\ScheduledJobs -Principal (New-ScheduledTaskPrincipal -Logontype Interactive -Userid $env:USERNAME)
+            Start-ScheduledTask -TaskName UpdateWSL2CustomKernel -TaskPath Microsoft\Windows\PowerShell\ScheduledJobs
     }
 } catch {
     Write-Output $_
