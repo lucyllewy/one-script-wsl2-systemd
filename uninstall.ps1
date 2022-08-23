45dd2184bedfe91683671d312f2fc10956c36617
Dani Llewellyn <diddledani@ubuntu.com>
2022-08-23 17:59:43 +0100

    Add Out-IniFile to uninstall.ps1
    Fixes #37

--- uninstall.ps1
+++ uninstall.ps1
@@ -195,6 +195,32 @@ function Write-IniOutput($InputObject)
     }
 }
 
+function Out-IniFile($InputObject, $FilePath)
+{
+    $outFile = New-Item -ItemType file -Path $Filepath -Force
+    foreach ($i in $InputObject.keys)
+    {
+        if (!($($InputObject[$i].GetType().Name) -eq "Hashtable"))
+        {
+            #No Sections
+            Add-Content -Path $outFile -Value "$i=$($InputObject[$i])"
+        } else {
+            #Sections
+            Add-Content -Path $outFile -Value "[$i]"
+            Foreach ($j in ($InputObject[$i].keys | Sort-Object))
+            {
+                if ($j -match "^Comment[\d]+") {
+                    Add-Content -Path $outFile -Value "$($InputObject[$i][$j])"
+                } else {
+                    Add-Content -Path $outFile -Value "$j=$($InputObject[$i][$j])"
+                }
+
+            }
+            Add-Content -Path $outFile -Value ""
+        }
+    }
+}
+
 $powershellProcess = (Get-Process -Id $PID).ProcessName + '.exe'
 
 if (-not [System.Environment]::Is64BitProcess) {
